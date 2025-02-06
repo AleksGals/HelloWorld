6428297ded099d069d2260c707a26984eeca7d3c
компьютер <git@github.com:AleksGals/HelloWorld.git>
2025-02-06 23:46:19 +0300

    Change language message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,75 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, Мир!");
-    }
-}
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        {
+
+            var result = (dog + 4);
+            System.out.println(result);
+        }
+
+        {
+            var result = (cat + 4);
+            System.out.println(result);
+        }
+
+        {
+            var result = (paper + 4);
+            System.out.println(result);
+        }
+        {
+
+            var result = (12 - 3.5);
+            System.out.println(result);
+        }
+        {
+            var result = (7.6 - 1.6);
+            System.out.println(result);
+        }
+        {
+
+        var result = (763793 - 7639);
+        System.out.println(result);
+            {
+            }
+            var friend = 19;
+            System.out.println(friend);
+            {
+            var result = (friend - 2);
+                {
+
+                }
+            System.out.println (result);}}}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
